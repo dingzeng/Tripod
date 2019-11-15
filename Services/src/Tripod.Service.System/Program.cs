e3da674c3c7efcd63201cc8260f9e4250a719cef
jerry.zeng <1090211579@qq.com>
2019-11-14 23:42:50 -0800

    GrpcServerBuilder

--- Services/src/Tripod.Service.System/Program.cs
+++ Services/src/Tripod.Service.System/Program.cs
@@ -2,11 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using AutoMapper;
-using Grpc.Core;
-using Microsoft.Extensions.Configuration;
 using Tripod.Framework.Common;
-using Tripod.Service.System.Mapping;
 
 namespace Tripod.Service.System
 {
@@ -14,46 +10,11 @@ namespace Tripod.Service.System
     {
         static void Main(string[] args)
         {
-            // 获取配置信息
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-            var configuration = builder.Build();
-
-            var options = new ConfigurationOptions() {
-                ConnectionString = configuration["connectionString"]
-            };
-
-            // 设置Grpc日志处理器
-            GrpcEnvironment.SetLogger(new GrpcLogger());
-
-            // 动态配置对象映射
-            var mappingTypes = Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(IMapping).IsAssignableFrom(t));
-            var mapperConfiguration = new MapperConfiguration(cfg => 
-            {
-                foreach (var m in mappingTypes)
-                {
-                    var typeName = m.FullName;
-                    var mapping = (IMapping)m.Assembly.CreateInstance(typeName);   
-                    mapping.Configure(cfg);
-                }
-            });
-            var mapper = mapperConfiguration.CreateMapper();
-
-            // Server
-            const int Port = 50054;
-            Server server = new Server
-            {
-                Services = { SystemSrv.BindService(new SystemService(mapper, options)) },
-                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
-            };
-            server.Start();
-
-            Console.WriteLine("系统服务监听端口： " + Port);
-            Console.WriteLine("任意键退出...");
-            Console.Read();
-
-            server.ShutdownAsync().Wait();
+            new GrpcServerBuilder()
+                .UseHost("localhost")
+                .UsePort(80054)
+                .AddService((options,mapper) => SystemSrv.BindService(new SystemService(mapper, options)))
+                .Start();
         }
     }
 }
